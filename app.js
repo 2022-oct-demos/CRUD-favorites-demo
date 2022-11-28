83bdadddd6fb52f83f8d4d34d0297bfb2a88ca79
maddenlockin <maddenlockin@gmail.com>
2022-11-28 08:11:58 -0800

    set-up html and supabase url & key

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 import './auth/user.js';
 
 /* Get DOM Elements */
+const form = document.querySelector('.create-form');
+// const deleteButton = document.querySelector('#delete-button');
+// const listEl = document.querySelector('.list');
+// const error = document.querySelector('#error');
 
 /* State */
 
